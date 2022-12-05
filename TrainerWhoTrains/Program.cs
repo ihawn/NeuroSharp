fcbce78b4866e5e6d6885a5323adf7402acdd98c
Isaac hawn <ihawn.pl15@gmail.com>
2022-12-04 18:31:40 -0600

    worked on web app

--- TrainerWhoTrains/Program.cs
+++ TrainerWhoTrains/Program.cs
@@ -11,16 +11,15 @@ namespace Trainer
     {
         static void Main(string[] args)
         {
-            LetterIdentificationTraining(20);
+            LetterIdentificationTraining(15);
         }
 
         static void LetterIdentificationTraining(int epochs)
         {
-            string path = @"C:\Users\Isaac\Downloads";
+            string path = @"C:\Users\Isaac\Documents\C#\NeuroSharp\Data\CharacterRecognition";
             string possibleChars = "abcdef";
             
-            List<Vector<double>> xTrain = new List<Vector<double>>();
-            List<Vector<double>> yTrain = new List<Vector<double>>();
+            List<(Vector<double>, Vector<double>)> data = new List<(Vector<double>, Vector<double>)>();
            
             foreach (string file in Directory.EnumerateFiles(path, "*.txt"))
             {
@@ -32,24 +31,32 @@ namespace Trainer
                 Vector<double> y = Vector<double>.Build.Dense(possibleChars.Length);
                 y[possibleChars.IndexOf(character)] = 1;
                 
-                xTrain.Add(x);
-                yTrain.Add(y);
+                data.Add((x, y));
             }
 
+            Random rand = new Random();
+            data = data.OrderBy(x => rand.Next()).ToList();
+
+            double trainSplit = 0.9;
+
+            List<Vector<double>> xTrain = 
+                data.Take((int)Math.Round(trainSplit * data.Count)).Select(x => x.Item1).ToList();
+            List<Vector<double>> yTrain = 
+                data.Take((int)Math.Round(trainSplit * data.Count)).Select(y => y.Item2).ToList();
+            
+            List<Vector<double>> xTest = 
+                data.Skip((int)Math.Round(trainSplit * data.Count)).Select(x => x.Item1).ToList();
+            List<Vector<double>> yTest = 
+                data.Skip((int)Math.Round(trainSplit * data.Count)).Select(y => y.Item2).ToList();
+
             Network network = new Network(xTrain[0].Count);
-            /*network.Add(new ConvolutionalLayer(kernel: 2, filters: 16, stride: 1, channels: 1));
-            network.Add(new ActivationLayer(ActivationType.ReLu));
-            network.Add(new MaxPoolingLayer(poolSize: 2));*/
-            network.Add(new ConvolutionalLayer(kernel: 2, filters: 16, stride: 2, channels: 1));
+            network.Add(new ConvolutionalLayer(kernel: 2, filters: 40, stride: 1, channels: 1));
             network.Add(new ActivationLayer(ActivationType.ReLu));
             network.Add(new MaxPoolingLayer(poolSize: 2));
-            network.Add(new ConvolutionalLayer(kernel: 2, filters: 4, stride: 1, channels: 16));
+            network.Add(new ConvolutionalLayer(kernel: 2, filters: 12, stride: 2, channels: 40));
             network.Add(new ActivationLayer(ActivationType.ReLu));
-           /* network.Add(new FullyConnectedLayer(512));
-            network.Add(new ActivationLayer(ActivationType.Tanh));
-            network.Add(new FullyConnectedLayer(256));
-            network.Add(new ActivationLayer(ActivationType.Tanh));*/
-            network.Add(new FullyConnectedLayer(196));
+            network.Add(new MaxPoolingLayer(poolSize: 2));
+            network.Add(new FullyConnectedLayer(128));
             network.Add(new ActivationLayer(ActivationType.Tanh));
             network.Add(new FullyConnectedLayer(64));
             network.Add(new ActivationLayer(ActivationType.Tanh));
@@ -62,24 +69,28 @@ namespace Trainer
             
             string modelJson = network.SerializeToJSON();
             File.WriteAllText(@"C:\Users\Isaac\Documents\C#\NeuroSharp\NeurosharpBlazorWASM\wwwroot\NetworkModels\characters_model.json", modelJson);
-            
-           /* int i = 0;
-            int wrongCount = 0;
-            foreach(var test in xTest)
+
+            if (trainSplit < 1)
             {
-                var output = network.Predict(test);
-                int prediction = output.ToList().IndexOf(output.Max());
-                int actual = yTest[i].ToList().IndexOf(yTest[i].Max());
-                Console.WriteLine("Prediction: " + prediction);
-                Console.WriteLine("Actual: " + actual + "\n");
+                int i = 0;
+                int wrongCount = 0;
+                foreach (var test in xTest)
+                {
+                    var output = network.Predict(test);
+                    int prediction = output.ToList().IndexOf(output.Max());
+                    int actual = yTest[i].ToList().IndexOf(yTest[i].Max());
+                    Console.WriteLine("Prediction: " + prediction);
+                    Console.WriteLine("Actual: " + actual + "\n");
 
-                if(prediction != actual)
-                    wrongCount++;
+                    if (prediction != actual)
+                        wrongCount++;
 
-                i++;
+                    i++;
+                }
+
+                double acc = (1d - ((double)wrongCount) / i);
+                Console.WriteLine("Accuracy: " + acc);
             }
-            double acc = (1d - ((double)wrongCount) / i);
-            Console.WriteLine("Accuracy: " + acc);*/
         }
     }
 }
